5f5be345bcdf3335bf2cba9a41d9f0b19736c343
Jorge Rodriguez <jorg.rodriguezc@outlook.com>
2025-08-20 16:40:19 -0500

    Implementa una función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = []
 
-// Implementa una función para agregar amigos
+//1 Implementa una función para agregar amigos
 // Desarrolla una función, que permita al usuario ingresar un nombre en el campo de texto y añadirlo a la lista de amigos creada anteriormente.
 
 // Tareas específicas:
@@ -21,5 +21,26 @@ function agregarAmigo() {
 
     amigos.push(nombre);
     input.value = '';
-    console.log('El nombre es: ' + amigos); // Para verificar que se ha añadido correctamente
+    //console.log('El nombre es: ' + amigos); // Para verificar que se ha añadido correctamente
+    actualizarLista(); // Llamar a la función para actualizar la lista de amigos
+    console.log('Lista de amigos actualizada:', amigos); // Para verificar que la lista se anida
+}
+
+//2 Implementa una función para actualizar la lista de amigos
+// Crea una función que recorra el array amigos y agregue cada nombre como un elemento <li> dentro de una lista HTML. Usa innerHTML para limpiar la lista antes de agregar nuevos elementos.
+
+// Tareas específicas:
+// Obtener el elemento de la lista: Utilizar document.getElementById() o document.querySelector() para seleccionar la lista donde se mostrarán los amigos.
+// Limpiar la lista existente: Establecer lista.innerHTML = "" para asegurarse de que no haya duplicados al actualizar.
+// Iterar sobre el arreglo: Usa un bucle for para recorrer el arreglo amigos y crear elementos de lista (<li>) para cada título.
+
+function actualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ''; // Limpiar la lista existente
+
+    amigos.forEach(function(amigo) {
+        let li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
 }
